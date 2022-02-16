1f5e67264b0253522684e5fb66318ad0914f99c6
Raju Yadav <Raju.Yadav@disney.com>
2022-02-16 13:25:10 -0600

    Added opentelimetry sdk

--- boot-otel-tempo-api/src/main/java/io/opentelemetry/example/flight/FlightController.java
+++ boot-otel-tempo-api/src/main/java/io/opentelemetry/example/flight/FlightController.java
@@ -1,28 +1,66 @@
 package io.opentelemetry.example.flight;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.Scope;
+import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.context.propagation.TextMapGetter;
+import io.opentelemetry.context.propagation.TextMapPropagator;
+import io.opentelemetry.example.opentelemetry.ExampleConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FlightController {
-	
-	 private static final Logger LOGGER = LoggerFactory.getLogger(FlightController.class);
-	
-	private FlightService flightService;
-	
-	public FlightController(FlightService flightService) {
-		this.flightService = flightService;
-	}
-	
-	@GetMapping("/flights")
-    public List<Flight> greeting(@RequestParam(value = "origin", defaultValue = "India") String origin) {
-    	LOGGER.info("Before Service Method Call");
-        return flightService.getFlights(origin);
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(FlightController.class);
+    OpenTelemetry openTelemetry = ExampleConfiguration.initOpenTelemetry();
+    private final Tracer tracer = openTelemetry.getTracer("Api:Call");
+
+    private FlightService flightService;
+
+    public FlightController(FlightService flightService) {
+        this.flightService = flightService;
+
+    }
+
+    @GetMapping("/flights")
+    public List<Flight> greeting(@RequestHeader(value = "traceparent") String traceparent, @RequestParam(value = "origin", defaultValue = "India") String origin) {
+        LOGGER.info("Before Service Method Call: " + traceparent);
+        ContextPropagators propagators = openTelemetry.getPropagators();
+        TextMapPropagator textMapPropagator = propagators.getTextMapPropagator();
+        Context context = textMapPropagator.extract(Context.current(), traceparent, new TextMapGetter<String>() {
+            @Override
+            public Iterable<String> keys(String s) {
+                LOGGER.info("key: " + s);
+                List<String> list = new ArrayList<>();
+                list.add(s);
+                return list;
+            }
+
+            @Override
+            public String get(String s, String s2) {
+                LOGGER.info("key1: " + s);
+                LOGGER.info("key2: " + s2);
+                return s;
+            }
+        });
+        Span span = tracer.spanBuilder("MyRequest").setParent(context).setSpanKind(SpanKind.SERVER).startSpan();
+        try (Scope ignored = span.makeCurrent()) {
+            return flightService.getFlights(origin);
+        } finally {
+            span.end();
+        }
     }
 
 }
---
+++ boot-otel-tempo-api/src/main/java/io/opentelemetry/example/opentelemetry/ExampleConfiguration.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.example.opentelemetry;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
+import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.exporter.logging.LoggingSpanExporter;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * All SDK management takes place here, away from the instrumentation code, which should only access
+ * the OpenTelemetry APIs.
+ */
+public class ExampleConfiguration {
+
+    /**
+     * Initializes the OpenTelemetry SDK with a logging span exporter and the W3C Trace Context
+     * propagator.
+     *
+     * @return A ready-to-use {@link OpenTelemetry} instance.
+     */
+   public static OpenTelemetry initOpenTelemetry() {
+        //Export traces to Jaeger
+        SdkTracerProvider sdkTracerProvider =
+                SdkTracerProvider.builder()
+                        .addSpanProcessor(SimpleSpanProcessor.create(new LoggingSpanExporter()))
+                        .build();
+
+        OpenTelemetrySdk sdk =
+                OpenTelemetrySdk.builder()
+                        .setTracerProvider(sdkTracerProvider)
+                        .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
+                        .build();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(sdkTracerProvider::close));
+        return sdk;
+    }
+}
